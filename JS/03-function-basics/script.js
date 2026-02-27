d7a9efc90a008117821bc669f8780e1a717fd8c6
Cameron L. <imcelarson@gmail.com>
2026-02-26 22:39:35 -0700

    Example on nullish coalescing operator

--- JS/03-function-basics/script.js
+++ JS/03-function-basics/script.js
@@ -18,3 +18,16 @@ function showMessage(from, text = "no text given") {
 }
 
 showMessage("Ann"); // Ann: no text given
+
+
+// Example using "nullish coalescing operator"
+function showCount(count) {
+  // if count is undefined or null, show "unknown"
+  console.log(count ?? "unknown");
+  // ?? is equivilant to result = (a !== null && a !== undefined) ? a : b;
+    // nullish coalescing operator (??) is a logical operator that returns its right-hand side operand when the left-hand side operand is null or undefined
+}
+
+showCount(0); // 0
+showCount(null); // unknown
+showCount(); // unknown
