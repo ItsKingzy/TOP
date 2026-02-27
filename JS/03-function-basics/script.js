33ebf0edba0d351ce69c2c46aecfcc78705b1ee7
Cameron L. <imcelarson@gmail.com>
2026-02-26 22:27:07 -0700

    First function example

---
+++ JS/03-function-basics/script.js
@@ -0,0 +1,12 @@
+/*
+- function parameter: animal
+- function argument: "Goat"
+*/
+
+function favoriteAnimal(animal) {
+    return animal + " is my favorite animal!";
+}
+
+const message = favoriteAnimal("Goat");  // function argument: "Goat"
+console.log(message);
+console.log(favoriteAnimal("Goat"));
