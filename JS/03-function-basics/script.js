e2e405dc09b43724bf02e390050c53760337ffcd
Cameron L. <imcelarson@gmail.com>
2026-02-26 22:29:28 -0700

    Example on default expression

--- JS/03-function-basics/script.js
+++ JS/03-function-basics/script.js
@@ -10,3 +10,11 @@ function favoriteAnimal(animal) {
 const message = favoriteAnimal("Goat");  // function argument: "Goat"
 console.log(message);
 console.log(favoriteAnimal("Goat"));
+
+
+// Example of default value
+function showMessage(from, text = "no text given") {
+  console.log( from + ": " + text );
+}
+
+showMessage("Ann"); // Ann: no text given
