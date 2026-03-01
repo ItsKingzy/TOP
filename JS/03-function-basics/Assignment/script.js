19803590e1af2f3aa82f3a8ea252996d79796cd6
Cameron L. <imcelarson@gmail.com>
2026-02-28 21:41:06 -0700

    Completed Q2

--- JS/03-function-basics/Assignment/script.js
+++ JS/03-function-basics/Assignment/script.js
@@ -1,6 +1,15 @@
 // Write a function called add7 that takes one number and returns that number + 7.
+    // add7(10) should return 17
 function add7(num) {
     return num + 7;
 }
 
-console.log(num7(10));
+console.log(add7(10));
+
+
+
+
+// Write a function called multiply that takes 2 numbers and returns their product.
+    // multiply(3, 2) should return 6
+let multiply = (num1, num2) => num1 * num2;
+console.log(multiply(3, 2));
