4806d446ada83be963104c840c139e3d3bc23195
Cameron L. <imcelarson@gmail.com>
2026-03-06 22:36:38 -0700

    Did filter function

--- JS/07-loops-and-arrays/script.js
+++ JS/07-loops-and-arrays/script.js
@@ -8,6 +8,7 @@ for (const cat of cats) {
   console.log(cat);  // Prints the value within the cats array
 }
 
+
 // Exmaple 2: Using map
 /*
 1. Create a function toUpper which returns the upper of a sting
@@ -15,7 +16,20 @@ for (const cat of cats) {
 3. Print output
 */
 function toUpper(string) {
-    return string.toUpper();
+    return string.toUpperCase();
 }
 const upperCats = cats.map(toUpper);
 console.log(upperCats);  // [ "LEOPARD", "SERVAL", "JAGUAR", "TIGER", "CARACAL", "LION" ]
+
+
+// Example 3: filter() method
+/*
+- filter takes in a function lCat and returns string for which the first letter starts will 'L'
+*/
+function lCat(cat) {
+  return cat.startsWith("L");
+}
+const filtered = cats.filter(lCat);
+console.log(filtered);  // [ "Leopard", "Lion" ]
+
+
