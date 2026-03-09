9a65ddfab58179cf7f1bd344eb76a25a0374a0f5
Cameron L. <imcelarson@gmail.com>
2026-03-08 23:43:48 -0600

    Will do this exercise later

--- JS/07-loops-and-arrays/script.js
+++ JS/07-loops-and-arrays/script.js
@@ -246,3 +246,33 @@ console.log(styles);
 // Step 5
 styles.unshift("Rap", "Reggae");
 console.log(styles);
+
+
+// Array exercise 2
+/*
+The input is an array of numbers, e.g. arr = [1, -2, 3, 4, -9, 6].
+
+The task is: find the contiguous subarray of arr with the maximal sum of items.
+
+Write the function getMaxSubSum(arr) that will return that sum.
+
+For instance:
+getMaxSubSum([-1, 2, 3, -9]) == 5 (the sum of highlighted items)
+getMaxSubSum([-1, 2, 3, -9, 11]) == 11
+getMaxSubSum([-2, -1, 1, 2]) == 3
+getMaxSubSum([100, -9, 2, -3, 5]) == 100
+getMaxSubSum([1, 2, 3]) == 6 (take all)
+*/
+
+/*
+Idea: 
+- loop add all positive numbers and store in a var.
+- in the loop, store the max number if arr[i] > sum of all posivie nums
+- skip all negative, if max = 0, and sumPositive = 0, return 0
+*/
+function getMaxSubSum(arr) {
+  lenArr = arr.length;
+  max = Math.max(arr);  // Max num in array
+  sumPositive = 0;  // Sum of all positive numbers in array
+
+}
