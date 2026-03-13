85dc84725f06b64b361851a571f8d7bf26f60a51
Cameron L. <imcelarson@gmail.com>
2026-03-12 22:56:42 -0600

    Example of triple evens

--- JS/07-loops-and-arrays/script.js
+++ JS/07-loops-and-arrays/script.js
@@ -486,3 +486,23 @@ console.log(str); // Bilbo # Gandalf # Nazgul
 
 // Visit this link for more info on arrays and methods:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+
+
+
+
+// triple evens example
+function sumOfTripledEvens(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    // Step 1: If the element is an even number
+    if (array[i] % 2 === 0) {
+      // Step 2: Multiply this number by three
+      const tripleEvenNumber = array[i] * 3;
+
+      // Step 3: Add the new number to the total
+      sum += tripleEvenNumber;
+    }
+  }
+  return sum;
+}
