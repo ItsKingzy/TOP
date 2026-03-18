3f594122c800fc1ffe772b02279b2251816845a5
Cameron L. <imcelarson@gmail.com>
2026-03-17 22:40:02 -0600

    Solved problem 1

---
+++ JS/practice-exercises/script.js
@@ -0,0 +1,9 @@
+// Q1: https://www.codecademy.com/resources/blog/10-javascript-code-challenges-for-beginners
+
+function printEven(n) {
+    for (let i = 0; i <= n; i++) {
+        console.log(i);
+    }
+}
+
+printEven(10);
