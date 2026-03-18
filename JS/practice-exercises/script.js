460928d036047d6d42f24723bf9bfb1752c00e06
Cameron L. <imcelarson@gmail.com>
2026-03-17 23:16:45 -0600

    SOlved problem 2

--- JS/practice-exercises/script.js
+++ JS/practice-exercises/script.js
@@ -7,3 +7,19 @@ function printEven(n) {
 }
 
 printEven(10);
+
+
+
+
+// Q2: https://www.codecademy.com/resources/blog/10-javascript-code-challenges-for-beginners
+function multiplicationTable(n) {
+    for (let i = 1; i <= n; i++) {
+        let output = "";
+        for (let j = 1; j <= n; j++) {
+            output += j*i + " ";
+        }
+        console.log(output);
+    }
+}
+
+multiplicationTable(10);
