1627784db541916f5ba9147847c4f88ddf452e52
Cameron L. <imcelarson@gmail.com>
2026-03-19 00:12:42 -0600

    Added a div to the container DOM

--- JS/08-DOM-manipulation-and-events/script.js
+++ JS/08-DOM-manipulation-and-events/script.js
@@ -50,8 +50,24 @@ const p = document.createElement("p");
 p.style.color = "red";
 p.textContent = "Hey I'm red!"
 container2.appendChild(p);
+
 // Add an <h3> with blue text that says “I’m a blue h3!”
 const h3 = document.createElement("h3");
 h3.textContent = "I'm a blue h3!"
 h3.style["color"] = "blue";
 container2.appendChild(h3);
+
+// Add a <div> with a black border and pink background color with the following elements inside of it:
+    // another <h1> that says “I’m in a div”
+    // a <p> that says “ME TOO!”
+    // Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
+const div2 = document.createElement("div");
+div2.setAttribute("style", "padding: 10px; border: solid; border-color: black; background-color: pink;")
+const h1 = document.createElement("h1");
+h1.textContent = "I'm in a div";
+h1.setAttribute("style", "color: green;")
+const p2 = document.createElement("p");
+p2.textContent = "ME TOO!";
+div2.appendChild(h1);
+div2.appendChild(p2);
+container2.appendChild(div2);
