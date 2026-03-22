06221b99b387ac489ba8242e342abaa5ebfff1ac
Cameron L. <imcelarson@gmail.com>
2026-03-21 18:16:22 -0600

    creating and placing nodes

--- JS/08-DOM-manipulation-and-events/assignment/script.js
+++ JS/08-DOM-manipulation-and-events/assignment/script.js
@@ -1,3 +1,13 @@
+// Doing some basic DOM manipulation
 const link = document.querySelector("#a2");
 link.textContent = "Mozilla Developer Network";
 link.href = "https://developer.mozilla.org";
+
+// Creating and placing new nodes
+const sect = document.querySelector("section");  // sect is a node
+const para = document.createElement("p");
+para.textContent = "We hope you enjoyed the ride."
+sect.appendChild(para);
+const text = document.createTextNode(" - the premier source for web development knowledge.");
+const linkPara = document.querySelector("p");
+linkPara.appendChild(text);
