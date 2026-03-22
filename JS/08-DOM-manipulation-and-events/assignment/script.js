123d5c396a7a9106f75df194783e5f2e3475f5da
Cameron L. <imcelarson@gmail.com>
2026-03-21 18:22:38 -0600

    Finised moving and removing elements

--- JS/08-DOM-manipulation-and-events/assignment/script.js
+++ JS/08-DOM-manipulation-and-events/assignment/script.js
@@ -11,3 +11,16 @@ sect.appendChild(para);
 const text = document.createTextNode(" - the premier source for web development knowledge.");
 const linkPara = document.querySelector("p");
 linkPara.appendChild(text);
+
+// Moving and removing elements
+sect.appendChild(linkPara); // Moves the linkPara node to the bottom of the section, this 
+                            // does NOT make a second copy of the node. 
+                            // (moves the "We hope you enjoyed the ride" to the top)
+sect.removeChild(linkPara); // Removes the linkPara from the DOM
+linkPara.remove();          // This works the exact same 
+                                // (Only differnce is that it refers itself)
+linkPara.parentNode.removeChild(linkPara); // .remove() is not supported in older browsers, 
+                                           // so this is the work around for that
+
+// Manipulating Styles
+
